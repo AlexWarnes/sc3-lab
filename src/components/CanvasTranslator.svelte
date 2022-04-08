919b392531f331397f3458d5bd0c3a615db84ac5
Alex Warnes <alex.s.warnes@gmail.com>
2022-04-08 12:14:46 -0500

    add click-to-select event to meshes, svelte-trois

--- src/components/CanvasTranslator.svelte
+++ src/components/CanvasTranslator.svelte
@@ -1,24 +1,33 @@
 <script lang="ts">
-	import * as SC from 'svelte-cubed';
-	import { scale, fly } from 'svelte/transition';
-	import ObjectPropEditor from './ObjectPropEditor.svelte';
-	import { canvas, selectedObjects } from '../data/store';
-	$: selected = $selectedObjects.length === 1 && $selectedObjects.includes($canvas.id);
+  import * as SC from "svelte-cubed";
+  import { scale, fly } from "svelte/transition";
+  import ObjectPropEditor from "./ObjectPropEditor.svelte";
+  import { canvas, selectedObjects } from "../data/store";
+  import { TroisProvider } from "svelte-trois";
+
+  function onClick(evt) {
+    console.log(evt);
+  }
+
+  $: selected =
+    $selectedObjects.length === 1 && $selectedObjects.includes($canvas.id);
 </script>
 
 {#if $canvas}
-	<SC.Canvas
-		antialias={$canvas.antialias}
-		physicallyCorrectLights={$canvas.physicallyCorrectLights}
-		background={$canvas.background}
-		shadows={$canvas.shadows}
-	>
-		<slot />
-	</SC.Canvas>
+  <SC.Canvas
+    antialias={$canvas.antialias}
+    physicallyCorrectLights={$canvas.physicallyCorrectLights}
+    background={$canvas.background}
+    shadows={$canvas.shadows}
+  >
+    <TroisProvider>
+      <slot />
+    </TroisProvider>
+  </SC.Canvas>
 
-	{#if selected}
-		<div class="editor-wrapper" in:fly={{x: -200}} out:scale>
-			<ObjectPropEditor sceneObj={$canvas} />
-		</div>
-	{/if}
+  {#if selected}
+    <div class="editor-wrapper" in:fly={{ x: -200 }} out:scale>
+      <ObjectPropEditor sceneObj={$canvas} />
+    </div>
+  {/if}
 {/if}
--- src/components/MeshTranslator.svelte
+++ src/components/MeshTranslator.svelte
@@ -1,29 +1,46 @@
 <script lang="ts">
-	import * as THREE from 'three';
-	import * as SC from 'svelte-cubed';
-	import { fly, scale } from 'svelte/transition';
-	import ObjectPropEditor from './ObjectPropEditor.svelte';
-	import type { LabMesh } from '../models/models';
-	import { selectedObjects } from '../data/store';
+  import * as THREE from "three";
+  import * as SC from "svelte-cubed";
+  import { fly, scale } from "svelte/transition";
+  import ObjectPropEditor from "./ObjectPropEditor.svelte";
+  import type { LabMesh } from "../models/models";
+  import { objIsSelected, selectedObjects } from "../data/store";
+  import { MouseEvents } from "svelte-trois";
 
-	export let idx: number;
-	export let mesh: LabMesh;
-	$: selected = $selectedObjects.length === 1 && $selectedObjects.includes(mesh.id);
+  export let idx: number;
+  export let mesh: LabMesh;
+
+  function onClick(evt) {
+    evt.stopPropagation();
+    if (objIsSelected(mesh.id)) {
+      selectedObjects.set([]);
+    } else {
+      selectedObjects.set([mesh.id]);
+    }
+  }
+
+  $: selected =
+    $selectedObjects.length === 1 && $selectedObjects.includes(mesh.id);
 </script>
 
 {#if mesh}
-	<SC.Mesh
-		geometry={mesh.geometry}
-		material={mesh.material}
-		position={mesh.position}
-		rotation={mesh.rotation}
-		scale={mesh.scale}
-		castShadow={mesh.castShadow}
-		receiveShadow={mesh.receiveShadow}
-	/>
-	{#if selected}
-		<SC.Primitive object={new THREE.AxesHelper(mesh.scale[0] + 2)} position={mesh.position} />
-		<!-- {:else if mesh.isHovered}
+  <MouseEvents {onClick}>
+    <SC.Mesh
+      geometry={mesh.geometry}
+      material={mesh.material}
+      position={mesh.position}
+      rotation={mesh.rotation}
+      scale={mesh.scale}
+      castShadow={mesh.castShadow}
+      receiveShadow={mesh.receiveShadow}
+    />
+  </MouseEvents>
+  {#if selected}
+    <SC.Primitive
+      object={new THREE.AxesHelper(mesh.scale[0] + 2)}
+      position={mesh.position}
+    />
+    <!-- {:else if mesh.isHovered}
 	<SC.Primitive
 		object={new THREE.AxesHelper(mesh.scale[0] + 2).setColors(
 			new THREE.Color(0x161616),
@@ -32,12 +49,12 @@
 		)}
 		position={[0, 0, 0]}
 	/> -->
-	{/if}
+  {/if}
 
-	<!-- MESH Control Panel only appears when mesh isSelected -->
-	{#if selected}
-		<div class="editor-wrapper" in:fly={{x: -200}} out:scale>
-			<ObjectPropEditor sceneObj={mesh} />
-		</div>
-	{/if}
+  <!-- MESH Control Panel only appears when mesh isSelected -->
+  {#if selected}
+    <div class="editor-wrapper" in:fly={{ x: -200 }} out:scale>
+      <ObjectPropEditor sceneObj={mesh} />
+    </div>
+  {/if}
 {/if}
